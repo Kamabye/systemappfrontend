4f3be0983e6642f820714ad7cdd71fbd8add4ace
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-07-16 12:22:42 -0600

    Commit

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/SYSTEM-APP'));
+app.use(express.static(__dirname + '/SYSTEM-APP'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/SYSTEM-APP/index.html'));
+    res.sendFile(path.join(__dirname + '/SYSTEM-APP/index.html'));
 });
 app.listen(process.env.PORT || 5000 || 8080);
