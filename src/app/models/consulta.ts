89713613da45fed3e328f4ca4e304c43b8d76bcd
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-17 15:17:05 -0600

    Angular 17.3.11

--- src/app/models/consulta.ts
+++ src/app/models/consulta.ts
@@ -1,43 +1,43 @@
 import { Paciente } from "./paciente";
 
-export interface Consulta {
-    idConsulta: number;
+export class Consulta {
+    idConsulta: number = 0;
 
-    totalDer : number;
-    totalIzq : number;
+    totalDer : number = 0.0;
+    totalIzq : number = 0.0;
 
-    totalAstigDer : number;
-    totalAstigIzq : number;
+    totalAstigDer : number = 0.0;
+    totalAstigIzq : number = 0.0;
 
-    totalAngDer : number;
-    totalAngIzq : number;
+    totalAngDer : number = 0.0;
+    totalAngIzq : number = 0.0;
 
-    subLejDer : number;
-    subLejIzq : number;
+    subLejDer : number = 0.0;
+    subLejIzq : number = 0.0;
 
-    subLejAstigDer : number;
-    subLejAstigIzq : number;
+    subLejAstigDer : number = 0.0;
+    subLejAstigIzq : number = 0.0;
 
-    subLejAngDer : number;
-    subLejAngIzq : number;
+    subLejAngDer : number = 0.0;
+    subLejAngIzq : number = 0.0;
 
-    addDer : number;
-    addIzq : number;
+    addDer : number = 0.0;
+    addIzq : number = 0.0;
 
-    dip : number;
+    dip : number = 0.0;
 
-    rx : string;
+    rx : string = "";
 
-    ocupacion : string;
+    ocupacion : string = "";
 
-    avizq : number;
-    avder : number;
+    avizq : number = 0.0;
+    avder : number = 0.0;
 
-    avderSnellen : string;
-    avizqSnellen : string;
+    avderSnellen : string = "";
+    avizqSnellen : string = "";
 
-    createdAt: Date;
-    modifiedAt: Date;
+    createdAt: Date = new Date();
+    modifiedAt: Date = new Date();
 
-    paciente : Paciente;
+    paciente : Paciente = new Paciente;
 }
---
+++ src/app/modules/home/consulta/formconsulta.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Paciente } from 'src/app/models/paciente';
+import { PacienteService } from 'src/app/services/paciente.service';
+
+import { Consulta } from 'src/app/models/consulta';
+import { ConsultaService } from 'src/app/services/consulta.service';
+
+
+
+import swal from 'sweetalert2';
+
+@Component({
+    selector: 'app-formconsulta',
+    templateUrl: './formconsulta.component.html'
+  })
+
+  export class FormconsultaComponent implements OnInit {
+
+    public consulta: Consulta = new Consulta();
+
+  constructor(private pacienteService: PacienteService, private consultaService: ConsultaService, private router: Router, private activateRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    console.info("FormconsultaComponent ngOnInit()")
+
+    this.cargarpaciente()
+    this.cargarconsulta()
+
+  }
+
+  cargarpaciente(): void {
+      console.info("FormpconsultaComponent cargarpaciente()")
+      this.activateRoute.params.subscribe(params => {
+  
+        let idPaciente = params['idPaciente']
+  
+        if (idPaciente) {
+          this.pacienteService.getPaciente(idPaciente).subscribe({
+            next: data => {
+              if (data.body !== null) {
+                this.paciente = data.body;
+                console.info(this.paciente);
+              } else {
+                console.error('El cuerpo de la respuesta es nulo.');
+              }
+  
+            },
+            error: err => {
+              swal.fire('Mensaje: ', `${err.error.mensaje}`, 'warning')
+              console.error("Error al obtener el paciente: ", err);
+            },
+            complete: () => {
+              console.log('Pacientes loaded');
+            }
+          });
+        }
+      });
+  
+  
+    }
+
+  }
