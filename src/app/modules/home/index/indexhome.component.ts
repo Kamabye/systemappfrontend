0eab03f624dd6e486527035b409152044a709d23
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-19 18:15:46 -0600

    Comit git src

---
+++ src/app/modules/home/index/indexhome.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indexhome',
+  templateUrl: './indexhome.component.html'
+})
+export class IndexHomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    console.info("IndexHomeComponent ngOnInit");
+  }
+}
