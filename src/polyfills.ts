f3b025e4d3ca3a9e9797ba51a5ec01a36676372e
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-20 21:50:03 -0600

    polyfills

---
+++ src/polyfills.ts
@@ -0,0 +1,37 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/weak-map';
+import 'core-js/es/set';
+
+/**
+ * If supporting IE 9 and need to support the use of `Element.prototype.closest`, you must also include this polyfill.
+ */
+// import 'element-closest';
+
+/***************************************************************************************************
+ * APPLICATION POLYFILLS
+ */
+
+/**
+ * Date, dates, currency and numbers.
+ * Required for internationalization.
+ */
+// import '@angular/common/locales/global/es'; // Ejemplo para español
+// import '@angular/common/locales/global/fr';
+// import '@angular/common/locales/global/de';
+// import '@angular/common/locales/global/ru';
