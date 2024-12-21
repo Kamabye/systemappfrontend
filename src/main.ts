463198a4a98423caab86ece15c923fde2bb05328
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-20 21:13:24 -0600

    Swal maints

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { AppModule } from './app/app.module';
 
 import { environment } from './environments/environment';
 
+import Swal from 'sweetalert2';
+
 if (environment.production) {
   enableProdMode();
 }
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+//platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => Swal.fire('Error al cargar los datos', `${err.error.mensaje}`, 'warning'));
