0cfcea3f6ad4bf00d7ddbffc6cddb388a15a6fd1
Carlos Hernandez <juancarloshdzvqz@gmail.com>
2024-12-26 19:07:02 -0600

    Datoscompletosconsulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
-
-
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -12,6 +10,12 @@ import { Logger } from '@angular/compiler-cli';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es'; // Importa el locale español
+import { DatePipe } from '@angular/common'; // Importa DatePipe
+
+registerLocaleData(localeEs, 'es'); // Registra el locale español con el ID 'es'
 
 @NgModule({
   declarations: [
@@ -30,7 +34,8 @@ import { AppComponent } from './app.component';
     })
   ],
   providers: [
-    
+    { provide: LOCALE_ID, useValue: 'es' }, // Proporciona el locale ID
+    DatePipe // Asegúrate de proporcionar DatePipe si lo usas en providers
   ],
   exports: [
     AppComponent
